34e40b0c5ae533354c500699602ae4c4b7412b34
Teotsky1 <mateo.arangog@upb.edu.co>
2022-11-14 16:00:15 -0500

    Completado
    Codigo comentado

--- main.c
+++ main.c
@@ -4,7 +4,22 @@
 #include <unistd.h>
 #include <string.h>
 #include <semaphore.h>
-#define Hilos 5
+
+
+
+/*
+
+INTEGRANTES DEL EQUIPO README_LINDO:
+
+-MATEO ARANGO
+-JULIANA OLARTE
+-FELIPE BARON
+
+*/
+
+
+//Definimos el numero de hilos que vamos a ejecutar
+#define NumeroHilos 5
 
 
 //Variable global, que representa el numero de tacos iniciales
@@ -17,45 +32,79 @@ pthread_mutex_t mxShared;
 /*EL metodo Make Tacos sirve para aumentar el numero de tacos, o sea el productor*/
 void* make_Tacos(void* args)
 {
+
     //Un al que se entra si o si
     while(1)
     {
+
         //Un mutex al que se le manda la direccion de memoria de la estructura pthread_mutex_t
         //Lo que permite usar el Lock para la variable Tacos
         pthread_mutex_lock(&mxShared);
+
+
         //Aumentamos la vairable tacos en 5
         tacos = tacos + 5;
+
+        
         //Un mutex al que se le manda la direccion de memoria de la estructura pthread_mutex_t
         //Lo que permite usar el unlock de la variable tacos
         pthread_mutex_unlock(&mxShared);
+
+
         //Luego imprimimos el numero de tacos que se hicieron
         printf("\n5 Tacos listos\n");
+
+
         //Sleep para parar el hilo 2 seg
         sleep(2);
+
+
         //Un condicional, en el que si hay mas de 300 tacos, termine el hilo
         if (tacos > 300)
         {
+
             //Retornar nulo, nos ayuda a salir del ciclo
             return NULL;
         }
     }
 }
 
+
+
 void* consume_Tacos(void* args)
 {
     //Un ciclo al que siempre vamos a entrar
     while(1)
     {
+
+
         //Un condicional para solo disminuir los tacos si 
         if (tacos > 0)
         {
+
+
+            //Un mutex al que se le manda la direccion de memoria de la estructura pthread_mutex_t
+            //Lo que permite usar el Lock para la variable Tacos
             pthread_mutex_lock(&mxShared);
+
+
+            //Disminuimos la variable Tacos en 1
             tacos = tacos - 1;
+
+
+            //Un mutex al que se le manda la direccion de memoria de la estructura pthread_mutex_t
+            //Lo que permite usar el unlock de la variable tacos
             pthread_mutex_unlock(&mxShared);
+
+
+            //Imprimimos el numero de tacos que se vendieron 
             printf("\nSe vendio 1 taco\n");
+
+
         }
         else
         {
+            //Si los tacos son 0 o menor que 0 se imprimir:
             printf("\nEspere por favor, estamos preparando los tacos\n");
         }
 
@@ -71,15 +120,31 @@ void* consume_Tacos(void* args)
     }
 }
 
+
+
+
+//un metodo que nos dirá cuando hay de inventario de tacos
 void* inventario_Tacos(void* args)
 {
+
+    //Un ciclo al que vamos a entrar 
     while(1)
     {
-        sleep(5);
+
+
+        //Vamos a imprimir cuantos tacos hay 
         printf("\nEn este momento hay: %i tacos en el inventario\n", tacos);
+
+
+        //Luego vamos a dormir el ciclo 5 seg
+        sleep(5);
+        
+        
+        //Cuando los tacos sean mayores a 300 
         if (tacos > 300)
         {
-        return NULL;
+            //vamos a salir del inventario de tacos
+            return NULL;
         }
     }
     
@@ -88,31 +153,50 @@ void* inventario_Tacos(void* args)
 
 int main(int argc, char const *argv[])
 {
-    pthread_t hilo[Hilos];
+    //Creamos los hilos, utilizando un Vector de Numero de hilos definido arriba
+    pthread_t hilo[NumeroHilos];
     
 
 
+    //Creamos cada hilo por aparte para que funcione:
 
+
+    /*El primer Hilo va a ser el Hilo productor*/
     pthread_create(&hilo[0], NULL, &make_Tacos, NULL);
     
+
+    /*este Hilo sera un consumidor*/
     pthread_create(&hilo[1], NULL, &consume_Tacos, NULL);
-    
+
+
+    /*este Hilo sera un consumidor*/
     pthread_create(&hilo[2], NULL, &consume_Tacos, NULL);
 
+
+    /*este Hilo sera un consumidor*/
     pthread_create(&hilo[3], NULL, &consume_Tacos, NULL);
     
+
+    //El 4to hilo nos dira cuantos tacos hay 
     pthread_create(&hilo[4], NULL, &inventario_Tacos, NULL);
 
     
-    for (int i = 0; i < Hilos; i++)
+    /*Creamos un ciclo para hacer el Join de los hilos*/
+    for (int i = 0; i < NumeroHilos; i++)
     {
+
+        //Si el Hilo que estamos mirando, es != 0
         if(pthread_join(hilo[i],NULL) != 0)
-        {
-            perror("Failed to join");
+        {   
+            //Se dara un error al hacer el join del hilo que estemos mirando
+            perror("Failed to join (hilo %i) ");
         }
     }
     
-
+    //Liberamos la memoria de &mxShared
     pthread_mutex_destroy(&mxShared);
+    
+
+    /*SI TODO SALE BIEN, EL PROGRAMA RETORNARA 0*/
     return 0;
 }
