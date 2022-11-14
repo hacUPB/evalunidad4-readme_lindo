2c736b7eda26da4c3a7e69f110f63f6f640c9bf8
Teotsky1 <mateo.arangog@upb.edu.co>
2022-11-14 15:09:45 -0500

    asda

--- main.c
+++ main.c
@@ -7,32 +7,47 @@
 #define Hilos 5
 
 
-
+//Variable global, que representa el numero de tacos iniciales
 int tacos = 100;
 
+/*Estructura Mutex que mas adelante utilizaremos para utiizar Loc y Unlock de la variable local*/
 pthread_mutex_t mxShared;
 
+
+/*EL metodo Make Tacos sirve para aumentar el numero de tacos, o sea el productor*/
 void* make_Tacos(void* args)
 {
+    //Un al que se entra si o si
     while(1)
     {
+        //Un mutex al que se le manda la direccion de memoria de la estructura pthread_mutex_t
+        //Lo que permite usar el Lock para la variable Tacos
         pthread_mutex_lock(&mxShared);
+        //Aumentamos la vairable tacos en 5
         tacos = tacos + 5;
+        //Un mutex al que se le manda la direccion de memoria de la estructura pthread_mutex_t
+        //Lo que permite usar el unlock de la variable tacos
         pthread_mutex_unlock(&mxShared);
+        //Luego imprimimos el numero de tacos que se hicieron
         printf("\n5 Tacos listos\n");
+        //Sleep para parar el hilo 2 seg
         sleep(2);
+        //Un condicional, en el que si hay mas de 300 tacos, termine el hilo
         if (tacos > 300)
         {
-        return NULL;
+            //Retornar nulo, nos ayuda a salir del ciclo
+            return NULL;
         }
     }
 }
 
 void* consume_Tacos(void* args)
 {
+    //Un ciclo al que siempre vamos a entrar
     while(1)
     {
-        if (tacos != 0)
+        //Un condicional para solo disminuir los tacos si 
+        if (tacos > 0)
         {
             pthread_mutex_lock(&mxShared);
             tacos = tacos - 1;
@@ -43,11 +58,15 @@ void* consume_Tacos(void* args)
         {
             printf("\nEspere por favor, estamos preparando los tacos\n");
         }
+
+
+        //Sleep para parar el hilo 2 seg
         sleep(2);
 
         if (tacos > 300)
         {
-        return NULL;
+            //Retornar nulo, nos ayuda a salir del ciclo
+            return NULL;
         }   
     }
 }
